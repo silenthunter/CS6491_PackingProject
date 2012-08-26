16b99cc23d076a0d4b3510097f51e5e9951c3ea5
silenthunter <silenthunter@gmail.com>
2012-08-26 04:15:44 -0400

    Remove files that were accidentally added earlier

--- IntersectionCount.java
+++
@@ -1,3 +0,0 @@
-public enum IntersectionCount {
-	ZERO, ONE, TWO, INFINITY;
-}
--- ShapeType.java
+++
@@ -1,3 +0,0 @@
-public enum ShapeType {
-	SHAPE, POINT, CIRCLE, LINE; // SHAPE doesn't mean anything. TODO: Change this odd implementation
-}
